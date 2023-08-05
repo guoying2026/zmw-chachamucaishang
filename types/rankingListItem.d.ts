9c1fa19e9129ad6f6861fbd07e7653435aba25e1
joytou <1540294142@qq.com>
2023-08-05 13:59:52 +0800

    添加信用排行榜列表项目的类型声明

---
+++ types/rankingListItem.d.ts
@@ -0,0 +1,12 @@
+export type RankingListItem = {
+  id: number;
+  company_name: string;
+  range: string;
+  address: string;
+  corporation: string;
+  credit_code: string;
+  score: number;
+  comment_count: number;
+  ask_count: number;
+  complaint_count: number;
+};
