6afb16474b92d1137dd5bf3c7a9d0fa491e9e69f
joytou <1540294142@qq.com>
2023-08-08 23:03:17 +0800

    添加地区数据，修改搜索结果页的区域筛选数据来源

--- pages/searchResult.vue
+++ pages/searchResult.vue
@@ -43,6 +43,50 @@ const areaList = ref<AreaListItem[]>([{
   }],
 }]);
 
+fetch('http://[::]:3000/area.json').then(res => res.json()).then(res => {
+  /* // 处理直辖市，把地级市级的“市辖区”删除，把区县级移到地级市级
+  res = res.map(item => {
+    if ([11, 12, 31, 50].includes(Number(item.code))) {
+      item.childs = item.childs[0].childs;
+      return item;
+    }
+    item.childs = item.childs.map(subitem => {
+      subitem.childs = [];
+      return subitem;
+    });
+    return item;
+  }); */
+  // 添加“全(省/市/自治区)”
+  res = res.map(item => {
+    let end = '';
+    if (item.name.includes('自治区')) {
+      end = '全自治区';
+    } else if (item.name.includes('市')) {
+      end = '全市';
+    } else {
+      end = '全省';
+    }
+    item.childs.unshift({
+      code: item.code,
+      name: item.name + end,
+      childs: [],
+    });
+    return item;
+  });
+  // 添加“全国”
+  res.unshift({
+    code: 0,
+    name: '全国',
+    childs: [{
+      code: 0,
+      name: '全国',
+      childs: [],
+    }],
+  });
+  console.log(res);
+  areaList.value = res;
+});
+
 const isShowAreaSelect = ref<boolean>(false)
 
 const areaFirstSelectedIndex = ref<number>(0)
@@ -114,22 +158,24 @@ function getGeoPosition() {
       <div @click="changeToLeaveMeClosestDistance" :class="'inline-flex flex-row justify-center items-center text-sm font-normal cursor-pointer' + (isLeaveMeClosestDistance ? ' font-orange' : '')">离我最近</div>
     </div>
     <!-- 区域筛选弹出框 -->
-    <div :class="'w-full ' + (isShowAreaSelect ? 'max-h-screen' : 'max-h-0') + ' inline-flex flex-row justify-start items-start text-sm font-normal overflow-hidden transition-all area-select-box'">
-      <div class="inline-flex flex-col justify-start items-center w-1/3 h-full overflow-x-hidden overflow-y-scroll">
-        <div v-for="(item, index) in areaList" @click="changeAreaFirstSelectedIndex(index)" :class="'inline-flex flex-col justify-center items-start w-full px-6 py-2 cursor-pointer' + (index === areaFirstSelectedIndex ? ' selected' : '') + (index === areaFirstSelectedIndex && areaFirstSelectedIndex > 0 ? ' font-orange' : '')">{{ item.name }}</div>
+    <div :class="'w-full md:w-1/2 ' + (isShowAreaSelect ? 'max-h-screen' : 'max-h-0') + ' inline-flex flex-row justify-start items-start text-sm font-normal pr-0.5 overflow-hidden transition-all area-select-box'">
+      <div class="inline-flex flex-col justify-start items-center w-1/3 md:w-1/2 h-full overflow-x-hidden overflow-y-scroll">
+        <div v-for="(item, index) in areaList" @click="changeAreaFirstSelectedIndex(index)" :class="'inline-flex flex-col justify-center items-start w-full px-6 md:px-2 py-2 cursor-pointer' + (index === areaFirstSelectedIndex ? ' selected' : '') + (index === areaFirstSelectedIndex && areaFirstSelectedIndex > 0 ? ' font-orange' : '')">{{ item.name }}</div>
       </div>
-      <div class="inline-flex flex-col justify-start items-center w-2/3 h-full overflow-x-hidden overflow-y-scroll">
-        <div v-for="(item, index) in areaList[areaFirstSelectedIndex].childs" @click="changeAreaSecondSelectedIndex(index)" :class="'inline-flex flex-col justify-center items-start w-full px-8 py-2 cursor-pointer' + (index === areaSecondSelectedIndex ? ' selected' : '') + (index === areaSecondSelectedIndex && areaFirstSelectedIndex > 0 ? ' font-orange' : '')">{{ item.name }}</div>
+      <div class="inline-flex flex-col justify-start items-center w-2/3 md:w-1/2 h-full overflow-x-hidden overflow-y-scroll">
+        <div v-for="(item, index) in areaList[areaFirstSelectedIndex].childs" @click="changeAreaSecondSelectedIndex(index)" :class="'inline-flex flex-col justify-center items-start w-full px-8 md:px-2 py-2 cursor-pointer' + (index === areaSecondSelectedIndex ? ' selected' : '') + (index === areaSecondSelectedIndex && areaFirstSelectedIndex > 0 ? ' font-orange' : '')">{{ item.name }}</div>
       </div>
     </div>
   </div>
-  <div class="inline-block w-full h-screen">
+  <div class="inline-block w-full min-h-screen mt-10 text-sm">
+    <div>为你找到了<span class="mx-1 font-orange">183949</span>条相关结果</div>
   </div>
 </template>
 
 <style scoped>
 .area-select-box {
-  height: 20vh;
+  height: 50vh;
+  background-color: rgb(30,30,30);
 }
 
 ::-webkit-scrollbar {
@@ -193,5 +239,11 @@ function getGeoPosition() {
 
 .font-orange {
   color: rgb(253,148,53);
+}
+
+@media (min-width: 768px) {
+  .area-select-box {
+    margin-left: 25%;
+  }
 }
 </style>
