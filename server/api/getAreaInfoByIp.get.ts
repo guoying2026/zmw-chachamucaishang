46c90d632404a997624db7d9abcb35c6b8720138
joytou <1540294142@qq.com>
2023-08-13 19:54:44 +0800

    解析ip地址信息更换为ip2region

--- server/api/getAreaInfoByIp.get.ts
+++ server/api/getAreaInfoByIp.get.ts
@@ -1,6 +1,11 @@
-import geoip from 'fast-geoip'
+import Searcher from './ip2region.class'
+import { AreaListItem } from '../../types/areaListItem';
+
+// 下载地址：https://gitee.com/lionsoul/ip2region/raw/master/data/ip2region.xdb
+const searcher = Searcher.newWithFileOnly('server/data/ip2region.xdb')
 
 export default defineEventHandler (async (e) => {
+  let ip = ''
   if (e.node.req.url) {
     let res: { [key: string]: any } = {};
     e.node.req.url
@@ -11,7 +16,56 @@ export default defineEventHandler (async (e) => {
       let value = item.substring(item.indexOf('=') + 1)
       res[key] = value
     })
-    return geoip.lookup(res.ip)
+    if (res.ip) ip = res.ip as string
   }
+  if (ip.trim() == '') {
+    ip = e.node.req.headers['x-forwarded-for'] as string // 判断是否有反向代理 IP
+    || e.node.req.connection.remoteAddress as string // 判断 connection 的远程 IP
+    || e.node.req.socket.remoteAddress as string // 判断后端的 socket 的 IP
+  }
+  return new Promise(resolve => {
+    searcher.search(res.ip).then((res: any) => {
+      let region: string = res.region as string
+      let regionArr: string[] = region.split('|')
+      let country: string = regionArr[0] as string
+      let province: string = regionArr[2] as string
+      let city: string = regionArr[3] as string
+      e.fetch('/api/areaData')
+      .then(r => r.json())
+      .then((r: {code: number, message: string, result: AreaListItem[]}) => {
+        let provinceCode = "0"
+        let cityCode = "0"
+        let adcode = "0"
+        let filterProvince = r.result.filter(item => item.name == province || item.name.includes(province))
+        if (filterProvince.length > 0) {
+          provinceCode = filterProvince[0].code.toString()
+          adcode = provinceCode.padEnd(6, '0')
+          let filterCity = filterProvince[0].childs.filter(item => item.name == city || item.name.includes(city))
+          if (filterCity.length > 0) {
+            cityCode = filterCity[0].code.toString()
+            adcode = cityCode.padEnd(6, '0')
+          }
+        }
+        adcode = adcode.padEnd(6, '0')
+        resolve({code: 200, message: '请求成功', result: {
+          origin:  res,
+          country: country,
+          province: province,
+          city: city,
+          province_code: provinceCode,
+          city_code: cityCode,
+          adcode: adcode,
+        }})
+      }).catch(e => {
+        resolve({code: 200, message: '请求成功', result: {
+          country: country,
+          province: province,
+          city: city,
+        }})
+      })
+    }).catch((err: any) => {
+      resolve(err)
+    })
+  })
   return 'error'
 })
--- server/api/myIp.get.ts
+++ server/api/myIp.get.ts
@@ -6,30 +6,4 @@ export default defineEventHandler (async (e) => {
     || e.node.req.connection.remoteAddress // 判断 connection 的远程 IP
     || e.node.req.socket.remoteAddress // 判断后端的 socket 的 IP
   };
-  const key = 'OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77';
-  // const key = '74XBZ-WJZCL-LMYP7-MYFWR-Z5E56-YDF2U';
-  const body = await e.fetch('https://apis.map.qq.com/ws/location/v1/ip?key=' + key, {
-    // headers: {
-    //   "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    //   "accept-language": "zh-CN,zh-TW;q=0.9,zh-HK;q=0.8,zh;q=0.7,en-US;q=0.6,en;q=0.5,zh-MO;q=0.4,en-GB;q=0.3",
-    //   "cache-control": "max-age=0",
-    //   "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
-    //   "sec-ch-ua-mobile": "?0",
-    //   "sec-ch-ua-platform": "\"Windows\"",
-    //   "sec-fetch-dest": "document",
-    //   "sec-fetch-mode": "navigate",
-    //   "sec-fetch-site": "same-site",
-    //   "sec-fetch-user": "?1",
-    //   "upgrade-insecure-requests": "1"
-    // },
-    // referrer: "https://lbs.qq.com/",
-    // referrerPolicy: "strict-origin-when-cross-origin",
-    // body: null,
-    // method: 'GET',
-    // mode: 'cors',
-    // credentials: 'include',
-  }).then(res => res.json()).then(res => {
-    e.node.res.end({code: 200, message: '请求成功', result: res})
-  })
-  return { body }
 })
