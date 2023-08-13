d3ec1b89de0473d4eb4b9dc043bf52a3cb407d78
joytou <1540294142@qq.com>
2023-08-13 15:18:39 +0800

    geoip-lite更换为fast-geoip

--- server/api/getAreaInfoByIp.get.ts
+++ server/api/getAreaInfoByIp.get.ts
@@ -1,7 +1,4 @@
-const baiduAK = 'Cl07wZmoUUkucWnl3xeXO2YA98X4OB9R';
-const tencentMapKey = 'OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77';
-
-import geoip from 'geoip-lite'
+import geoip from 'fast-geoip'
 
 export default defineEventHandler (async (e) => {
   if (e.node.req.url) {
@@ -14,21 +11,7 @@ export default defineEventHandler (async (e) => {
       let value = item.substring(item.indexOf('=') + 1)
       res[key] = value
     })
-
-    geoip.reloadDataSync()
-
     return geoip.lookup(res.ip)
-    
-    // return new Promise(resolve => {
-      /* e.fetch('https://apis.map.qq.com/ws/location/v1/ip?ip=' + res.ip + '&key=' + tencentMapKey)
-      .then(res => res.json())
-      .then(res => resolve(res))
-      .catch(err => resolve(err)) */
-      /* e.fetch('https://api.map.baidu.com/location/ip?ip=' + res.ip + '&coor=bd09ll&ak=' + baiduAK)
-      .then(res => res.json())
-      .then(res => resolve(res))
-      .catch(err => resolve(err)) */
-    // })
   }
   return 'error'
 })
