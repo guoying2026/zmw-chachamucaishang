d289fbf33bd9f79bb19c8e26cc48ab246e9f97e3
guoying <2473548838@qq.com>
2023-08-14 09:13:11 +0800

    chongtu

--- components/Complaint.vue
+++ components/Complaint.vue
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 <!--<template>-->
 <!--  <img src="https://zhenmuwang.oss-cn-beijing.aliyuncs.com/sell_answer_img__miniapp_3cef8124-864e-4abd-b190-966c6d1a69f9.png">-->
 <!--</template>-->
+=======
+<template>
+<!--  <img src="">-->
+</template>
+>>>>>>> d7289f1 (评论)
