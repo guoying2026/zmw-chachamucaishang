665a1e0d0cf1ad1863f3c0b108a66f91bb4ba592
joytou <1540294142@qq.com>
2023-08-31 14:08:40 +0800

    修改提交的docker镜像

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ docker build -f docker/Dockerfile -t ccmcs:latest .
 
 dockerImageId=`docker images | grep ccmcs | awk '{print $3}'`
 
-docker tag $dockerImageId registry.cn-beijing.aliyuncs.com/jryhd/zmw-chachamucaishang:latest
+docker tag $dockerImageId registry.cn-beijing.aliyuncs.com/jryhd/zmw-pingfen:latest
 
-docker push registry.cn-beijing.aliyuncs.com/jryhd/zmw-chachamucaishang:latest
+docker push registry.cn-beijing.aliyuncs.com/jryhd/zmw-pingfen:latest
