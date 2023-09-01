836277b31fc014e8256fcb86b34a0d37110a4438
joytou <1540294142@qq.com>
2023-09-01 16:23:47 +0800

    移动端顶部搜索框修改为跳转到搜索页；修复移动端首页的搜索框在部分机型上的布局错误的问题

--- pages/index.vue
+++ pages/index.vue
@@ -121,7 +121,7 @@ nuxtApp.hook('page:finish', () => {
     <p class="text-sm sm:text-base md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl text-center font-medium tracking-widest m-4 mb-8 mx-auto whitespace-nowrap top-subtitle">助力检索木材交易隐患，降低木材交易风险</p>
     <!-- 搜索框 -->
     <div @click.stop="isMobile?searchBoxClickHandle():''" class="relative inline-flex justify-center w-full md:w-96 2xl:w-1/3 text-base search-box">
-      <input :disabled="isMobile" class="w-4/5 h-14 p-4 px-2 md:pl-10 pr-5 text-sm md:text-base xl:text-xl bg-white text-black search-text" type="search" placeholder="请输入企业名、人名等关键词查询" ref="searchTextRef" v-model="searchInputText" @keyup.enter="searchButtonHandle" />
+      <input :disabled="isMobile" class="w-4/5 h-14 p-4 px-2 md:pl-10 pr-5 text-sm md:text-base xl:text-xl bg-white text-black disabled:bg-white search-text" type="search" placeholder="请输入企业名、人名等关键词查询" ref="searchTextRef" v-model="searchInputText" @keyup.enter="searchButtonHandle" />
       <!-- 搜索图标 -->
       <svg class="absolute left-3 hidden md:inline-block w-5 h-14 search-icon" style="color: rgb(153,153,153);" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 1024 1024"><path fill="currentColor" d="M1014.64 969.04L703.71 656.207c57.952-69.408 92.88-158.704 92.88-256.208c0-220.912-179.088-400-400-400s-400 179.088-400 400s179.088 400 400 400c100.368 0 192.048-37.056 262.288-98.144l310.496 312.448c12.496 12.497 32.769 12.497 45.265 0c12.48-12.496 12.48-32.752 0-45.263zM396.59 736.527c-185.856 0-336.528-150.672-336.528-336.528S210.734 63.471 396.59 63.471c185.856 0 336.528 150.672 336.528 336.528S582.446 736.527 396.59 736.527z"/></svg>
       <!-- 叉叉图标 -->
@@ -213,6 +213,7 @@ nuxtApp.hook('page:finish', () => {
 }
 
 .search-text {
+  border-radius: 0px;
   border-top-left-radius: 5px;
   border-bottom-left-radius: 5px;
   transition: all 0s 150ms;
